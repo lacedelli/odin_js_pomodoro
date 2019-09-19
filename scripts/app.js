3cf5729e83e5f76845bc9e921380c71da76f611c
Lacedelli <lacedelli@outlook.com>
2019-09-19 17:38:23 -0500

    revamp app structure with TODOs and simple code hookups with html buttons

--- scripts/app.js
+++ scripts/app.js
@@ -4,27 +4,50 @@ const workTimer = document.querySelector(".work .timer p");
 const restTimer = document.querySelector(".rest .timer p");
 
 // TODO Do initial setup on the display of times and variables
-var workTime 
-var restTime 
-var activationTime 
+var displayTime = 25 
+var timeInterval = 0 
 
 // TODO Make a function that takes n parameters: the timer to be modified,
 // the operand to use on the number, and the node whose innerHTML is to be
 // changed
+function timerController(number, operand, node) {
+	if(operand === "add") {
+		
+	} else if(operand === "sub") {
 
-// TODO Make functions for each of the panel buttons
+	}
+}
 
+// TODO Make functions for play button
+function play() {
+	console.log("henlo, play");
+}
+
+// TODO Make a function for the pause button
+function pause() {
+	console.log("helno, pause");
+}
+
+// TODO Make a function for the stop button
+function stop() {
+	console.log("henlo, stop");
+}
+
+// TODO Make a function for the reset button
+function reset() {
+	console.log("henlo, reset");
+}
 
 // TODO	Make a handler function that formats time outputs into proper strings
 // for display on the webpage
+function parseToStr() {
+
+}
 
 // TODO Make a handler function that takes in text strings from the page and
 // converts them into integers for the code to work with
+function parseToInt() {
 
-function setGoalTime(time) {
-	var goalTime = new Date();
-	goalTime.setMinutes(goalTime.getMinutes() + time);
-	return goalTime;
 }
 
 // Math.floor(Date.now() /1000/60 %60) formula for minutes
